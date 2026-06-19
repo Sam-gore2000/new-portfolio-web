7b7bbe2bd229f891219be3fcfefc98f5471f3146
Samadhan Gore <Samadhan.Gore@LionReachMedia.local>
2026-06-19 13:29:08 +0530

    update github url

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
     {/* Social Icons */}
     <div className="hidden md:flex space-x-4">
       <a
-        href="https://github.com/sam-gore34/"
+        href="https://github.com/Sam-gore2000/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-gray-300 hover:text-[#8245ec]"
